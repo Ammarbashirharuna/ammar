de8b2aa47a272b137b62f19995721bf71e9bcc0e
Ammarbashirharuna <ammarbashirharuna2020@gmail.com>
2023-06-04 03:04:00 -0700

    ammar

---
+++ ammar.js
@@ -0,0 +1,72 @@
+console.log('ammar');
+const qote = document.getElementsByClassName('qote');
+
+
+console.log(qote);
+const skills = document.getElementsByClassName('skills');
+console.log(skills);
+
+let time = 'ammar';
+console.log(time);
+let kaunata = 'nabeela';
+if ('ammar'){
+    console.log('am yours')
+}
+
+ else 
+console.log('abarsona');
+let user = 'customer';
+console.log(`am sorry my ${user}`)
+let timeOfWorkOut = 9;
+if(timeOfWorkOut === 6){
+    console.log('time yanda mahimmanci')
+      
+} else if(timeOfWorkOut === 8){
+    console.log('hello boss still you have two hours')
+} else if(timeOfWorkOut === 9){
+    console.log('hello boss start perparing you only have one more hour')
+}else if(timeOfWorkOut === 6){
+    console.log('boss you have to helf your wife right know because she is tired')
+} else if(timeOfworkOut === 5){
+    console.log('hello boss wake up know')
+} else{
+    console.log('today no workout because is weekend')
+};
+
+function getgreeting() {
+    console.log('nabeela na ykk ne fatana kina lafiya');
+
+}
+  getgreeting();
+  function sayThanks(name) {
+        console.log('thank  your purchase '+ name +'we appreciate' );
+  }
+  sayThanks('Ammar');
+   
+
+  function  makeShopList(item1 = 'milk', item2 = 'butter',
+  item3 = 'wake') {
+    console.log(`Remember to buy ${item1}`)
+    console.log(`Remember to buy ${item2}`)
+    console.log(`Remember to buy ${item3}`)
+
+  }
+ 
+makeShopList();
+function goToWork(){
+    console.log('boss move on');
+}
+goToWork('we move')
+function sayThanks(){
+    console.log('thank you for your purchase call again');
+}
+function weHaveAlot(){
+    console.log('we have alot of product for you have you will be back again')
+}
+sayThanks();
+weHaveAlot();
+
+
+
+
+
